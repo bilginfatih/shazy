e214139bc4edec3c3b27d3285f0bc883e18e2caa
connetium-fatih <fatih16bilgin@gmail.com>
2023-08-19 06:20:39 +0300

    lokasyon revize

--- lib/pages/home/home_screen_transport.dart
+++ lib/pages/home/home_screen_transport.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart' as ph;
 
 class HomeScreenTransport extends StatefulWidget {
   HomeScreenTransport({Key? key}) : super(key: key);
@@ -14,26 +15,63 @@ class HomeScreenTransport extends StatefulWidget {
 class _HomeScreenTransportState extends State<HomeScreenTransport> {
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
   final Location _location = Location();
-  LocationData? _currentLocation;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
+  bool _locationPermissionGranted = false;
+
   @override
   void initState() {
     super.initState();
     _getLocation();
-    Timer.periodic(const Duration(seconds: 5), (Timer t) => _getLocation());
+    _getLocationPermission(); // İzin kontrolü eklendi
+    _subscribeToLocationChanges(); // Geolocation Aboneliği eklendi
+  }
+
+  // İzinleri kontrol eden fonksiyon
+  Future<void> _getLocationPermission() async {
+    ph.PermissionStatus status = await Permission.locationWhenInUse.request();
+    setState(() {
+      _locationPermissionGranted = status.isGranted;
+    });
+    if (status.isGranted) {
+      print('Lokasyon izni verildi.');
+    } else if (status.isDenied) {
+      print('Lokasyon izni verilmedi.');
+    } else if (status.isPermanentlyDenied) {
+      print('Lokasyon izni kalıcı olarak rededildi.');
+      _showPermissionSettingsDialog(); // Ayarlara gitme işlemi için fonksiyonu çağır
+    }
+  }
+
+  // Ayarlara gitme işlemi için bir diyalog gösteren fonksiyon
+  void _showPermissionSettingsDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Lokasyon İzinleri'),
+          content: Text('Uygulamanın konum izni gerektiği için ayarlara gitmek istiyor musunuz?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                ph.openAppSettings(); // Ayarlara gitmek için izinleri ayarlar
+                Navigator.of(context).pop();
+              },
+              child: const Text('Ayarlara Git'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   Future<void> _getLocation() async {
     try {
-      final LocationData locationData = await _location.getLocation();
-      setState(() {
-        _currentLocation = locationData;
-      });
+      setState(() {});
     } catch (e) {
       print('Could not get location: $e');
       ScaffoldMessenger.of(context).showSnackBar(
@@ -45,28 +83,25 @@ class _HomeScreenTransportState extends State<HomeScreenTransport> {
     }
   }
 
-  Future<void> _animateToUser() async {
-    final GoogleMapController controller = await _controller.future;
-    LocationData pos = await Location().getLocation();
-    CameraPosition newPos = CameraPosition(
-      target: LatLng(pos.latitude ?? 0, pos.longitude ?? 0),
-      zoom: 17.0,
-    );
-    controller.animateCamera(CameraUpdate.newCameraPosition(newPos));
+  // onLocationChanged'e abone olunarak konum güncellemeleri alınır
+  void _subscribeToLocationChanges() {
     _location.onLocationChanged.listen(
       (LocationData currentLocation) {
         print('Current location: ${currentLocation.latitude}, ${currentLocation.longitude}');
-        setState(() {
-          _currentLocation = currentLocation;
-        });
-
-        CameraPosition newPosition = CameraPosition(
-          target: LatLng(currentLocation.latitude ?? 0, currentLocation.longitude ?? 0),
-          zoom: 17.0,
-        );
-        controller.animateCamera(CameraUpdate.newCameraPosition(newPosition));
+        setState(() {});
+        _animateToUser(currentLocation.latitude ?? 0, currentLocation.longitude ?? 0);
       },
     );
+  }
+
+  // Kullanıcının konumunu harita üzerinde takip etmek için kamera animasyonu yapar
+  Future<void> _animateToUser(double latitude, double longitude) async {
+    final GoogleMapController controller = await _controller.future;
+    CameraPosition newPosition = CameraPosition(
+      target: LatLng(latitude, longitude),
+      zoom: 17.0,
+    );
+    controller.animateCamera(CameraUpdate.newCameraPosition(newPosition));
   }
 
   @override
@@ -76,7 +111,6 @@ class _HomeScreenTransportState extends State<HomeScreenTransport> {
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
-          _animateToUser();
         },
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
