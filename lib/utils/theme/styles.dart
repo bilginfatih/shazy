faa2781732f432e29a49b53bf5f70f0887e06109
Muhammed Halil Akkaynak <55804472+mhakkaynak@users.noreply.github.com>
2023-11-07 22:45:57 +0300

    syntax hatası düzeltildi :)

--- lib/utils/theme/styles.dart
+++ lib/utils/theme/styles.dart
@@ -1,4 +1,4 @@
-eaimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import '../extensions/context_extension.dart';
--- lib/utils/theme/themes.dart
+++ lib/utils/theme/themes.dart
@@ -1,4 +1,4 @@
-simport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class AppThemes {
